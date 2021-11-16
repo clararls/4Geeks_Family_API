36af9987beef4bdbb58940f1ec5c8d8a8ecfe0c7
Clara Rojano León-Salas <clararojanols@gmail.com>
2021-11-16 20:21:42 +0000

    -

--- src/datastructures.py
+++ src/datastructures.py
@@ -8,12 +8,33 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{
+            'id' : 1,
+            'first_name' : 'John',
+            'last_name' : self.last_name,
+            'age' : '33 Years old',
+            'lucky_numbers' : [7,13,22],
+        },
+        {
+            'id' : 2,
+            'first_name' : 'Jane',
+            'last_name' : self.last_name,
+            'age' : '35 Years old',
+            'lucky_numbers' : [10,14,3],
+        },
+        {
+            'id' : 3,
+            'first_name' : 'Jimmy',
+            'last_name' : self.last_name,
+            'age' : '5 Years old',
+            'lucky_numbers' : [1],
+        },]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,16 +42,31 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        person = {
+            "id": self._generateId(),
+            "first_name": member,
+            "last_name": last_name,
+        }
+        self._members.append(person)
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for position in range(len(self._members)):
+            if self._members[position]["id"] == id:
+                self._members.pop(position)
+
+    def update_member(self, id, member):
+        for position in range(len(self._members)):
+            if self._members[position]["id"] == id:
+                self._members.update(position)
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        filtered_member = list(
+            filter(lambda member: member["id"] == id, self._members))
+        return filtered_member
 
     # this method is done, it returns a list with all the family members
+
     def get_all_members(self):
         return self._members
